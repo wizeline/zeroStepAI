0b78257818b0089ddf4b5b5e15ca686cdc5f579e
VictorG30 <laurencio.garcia@wizeline.com>
2024-04-12 12:05:54 -0600

    adding more scenarios

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,37 @@ require('dotenv').config()
 
 const url = 'https://www.saucedemo.com/'
 
-test.only('Purshace article', async ({ page }) => {
+test('Login bad credentials', async ({ page }) => {
+  await ai('go to this page https://www.saucedemo.com/', { page, test })
+  await ai('insert any username', { page, test })
+  await ai('insert any password', { page, test })
+  await ai('click buton login', { page, test })
+  var message = await ai('get the value from the error message', { page, test })
+  console.log(message)
+
+  expect(message).toContain('not match')
+
+  await page.pause()
+
+});
+
+test('Search the most expensive article ', async ({ page }) => {
+  await ai('go to this page https://www.saucedemo.com/', { page, test })
+  await ai('insert username as standard_user', { page, test })
+  await ai('insert password as secret_sauce in the password field', { page, test })
+  await ai('click buton login', { page, test })
+  await ai('order product by low to high', { page, test })
+  await ai('click in the most expensive article', { page, test })
+  var price = await ai('get value of the price', { page, test })
+  console.log(price)
+  
+  expect(price).toEqual('$ 49.99')
+
+  await page.pause()
+
+});
+
+test('Purshace article', async ({ page }) => {
   await ai('go to this page https://www.saucedemo.com/', { page, test })
   await ai('insert username as standard_user', { page, test })
   await ai('insert password as secret_sauce in the password field', { page, test })
@@ -21,5 +51,26 @@ test.only('Purshace article', async ({ page }) => {
   await page.pause()
 
 });
+
+test.only('Purshace with missing requiered fields', async ({ page }) => {
+  await ai('go to this page https://www.saucedemo.com/', { page, test })
+  await ai('insert username as standard_user', { page, test })
+  await ai('insert password as secret_sauce in the password field', { page, test })
+  await ai('click buton login', { page, test })
+  await ai('go to te cart', { page, test })
+  await ai('checkout purchase', { page, test })
+  await ai('click continue button', { page, test })
+  var value = await ai('get the css locator from the error message', { page, test })
+  console.log(value)
+
+  const element = await page.locator(value)
+  expect(element).toBeVisible()
+
+  await page.pause()
+
+});
+
+
+
 
 
