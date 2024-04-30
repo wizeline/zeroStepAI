8b411a758bb5745ad68b9ac0741b2347774f77e2
VictorG30 <laurencio.garcia@wizeline.com>
2024-04-30 01:00:32 -0600

    wp

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ import { ai } from '@zerostep/playwright'
 require('dotenv').config()
 
 test.use({ viewport: { width: 1700, height: 900 } });
-test.describe.serial('UX/UI', () => {
+test.describe.parallel('UX/UI', () => {
 
   test.beforeEach(async ({ page }) => {
               
@@ -16,16 +16,16 @@ test('Login bad credentials', async ({ page }) => {
   await ai('insert any username', { page, test })
   await ai('insert any password', { page, test })
   await ai('click buton login', { page, test })
-  var message = await ai('get the value from the error message', { page, test })
-  console.log(message)
+  var statusLogin = await ai('was the login succesful?', { page, test })
+  console.log(statusLogin)
 
-  expect(message).toContain('not match')
+  expect(statusLogin).toEqual(false)
 
   await page.pause()
 
 });
 
-test('Search the most expensive article ', async ({ page }) => {
+test('Search the most expensive article', async ({ page }) => {
   await ai('go to this page https://www.saucedemo.com/', { page, test })
   await ai('insert username as standard_user', { page, test })
   await ai('insert password as secret_sauce in the password field', { page, test })
@@ -33,7 +33,7 @@ test('Search the most expensive article ', async ({ page }) => {
   await ai('order product price high to low', { page, test })
   await page.pause()
 
-  var price = await ai('get value of the price', { page, test })
+  var price = await ai('get the price of the first article', { page, test })
   console.log(price)
   
   expect(price).toEqual('$ 49.99')
@@ -52,6 +52,7 @@ test('Purchase article', async ({ page }) => {
   await ai('fill form with real information', { page, test })
   await ai('click continue button', { page, test })
   await ai('finish purchase', { page, test })
+
   const element = await page.locator('.checkout_complete_container')
   expect(element).toBeVisible()
 
@@ -67,6 +68,7 @@ test('Purshace with missing requiered fields', async ({ page }) => {
   await ai('go to te cart', { page, test })
   await ai('checkout purchase', { page, test })
   await ai('click continue button', { page, test })
+
   var value = await ai('get the css locator from the error message', { page, test })
   console.log(value)
 
